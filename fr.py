c7f3267bb6ee5f377907e9961c05d631a1647c9d
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-08-10 20:52:34 +0530

    py fr program 3

--- fr.py
+++ fr.py
@@ -1,12 +1,24 @@
-totalFeet = 60
-up = 0
-down = 0.5
-day =0
-while up < 60:
-    up = up + 2
-    day = day + 1
-    up = up - down
-print(day)
-print(up)
-print(down)
+# totalFeet = 60
+# up = 0
+# down = 0.5
+# day =0
+# while up < 60:
+#     up = up + 2
+#     day = day + 1
+#     up = up - down
+# print(day)
+# print(up)
+# print(down)
 
+# ex : 02
+
+
+totalFeet = 60
+up = 2
+down = 0.5
+day = 0
+while totalFeet > 0:
+    totalFeet = totalFeet - up + down
+    day = day + 1
+
+print(day)
