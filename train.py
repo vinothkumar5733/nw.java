c34fe8bd74d9e09558dcf33fa226dead7fffe99d
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-08-12 21:09:05 +0530

    train py

--- train.py
+++ train.py
@@ -1,13 +1,14 @@
 station = 1
 last_station = station
-first_station = station
 while station <= 80:
     if station % 3 == 0 and station % 5 == 0:
         last_station = station   #15   30   45  60  75
-        if first_station == station:
-            first_station = station
+        if first_station == :
+            print('first station is: ', station)
+
         # print(last_station)
         print(station)
     station+=1
 
+print("First Common Station is", first_station)
 # print("Last Common Station is", last_station)
