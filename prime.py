ed818673afa4fae6cdf33430aea2cf7b9f098617
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-08-15 22:31:09 +0530

    prime no print & different 2

--- prime.py
+++ prime.py
@@ -1,43 +1,43 @@
-p=7
-d=2
-if d%p == 0:
-    print('not a prime')
-    d+=1
-else:
-    print(d)
-    print('prime')
+# p=7
+# d=2
+# if d%p == 0:
+#     print('not a prime')
+#     d+=1
+# else:
+#     print(d)
+#     print('prime')
 
 
-# # ex:02
+# # # ex:02
 
-n = 1319
-d=2
-while d<n:
-    if n%d ==0:
-        print('not a prime')
-        break
-    d+=1
-else:
-    print(n)
-    print('prime')
+# n = 1319
+# d=2
+# while d<n:
+#     if n%d ==0:
+#         print('not a prime')
+#         break
+#     d+=1
+# else:
+#     print(n)
+#     print('prime')
 
-# ex:03
+# # ex:03
 
-n = 1319
-d = 2
-while d <= n//2:
-    if n%d == 0:
-        print('not a prime')
-    d+=1
-else:
-    print(d)
-    print('prime')
+# n = 1319
+# d = 2
+# while d <= n//2:
+#     if n%d == 0:
+#         print('not a prime')
+#     d+=1
+# else:
+#     print(d)
+#     print('prime')
 
 
-# ex:04
+# # ex:04
 
 num = 1024
 div = 3
 while div<=2:
     if num *d !=0:
-        print
+        print('prime')
