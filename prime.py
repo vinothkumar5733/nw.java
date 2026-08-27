b23650a535fef1822d14b404397e929eb7e7e35c
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-08-27 22:39:40 +0530

    new start

--- prime.py
+++ prime.py
@@ -111,24 +111,3 @@ while mul>=1:
     if mul *i ==0:
         print("prime")
 
-def checkLuhn(cardNo):
-    
-    nDigits = len(cardNo)
-    nSum = 0
-    isSecond = False
-    
-    for i in range(nDigits - 1, -1, -1):
-        d = ord(cardNo[i]) - ord('0')
-    
-        if (isSecond == True):
-            d = d * 2def checkLuhn(cardNo):
-    
-    nDigits = len(cardNo)
-    nSum = 0
-    isSecond = False
-    
-    for i in range(nDigits - 1, -1, -1):
-        d = ord(cardNo[i]) - ord('0')
-    
-        if (isSecond == True):
-            d = d * 2
