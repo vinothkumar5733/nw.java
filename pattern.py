685f21b59ca600af6b3599c181fc984ca7ce0223
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-08-31 21:17:53 +0530

    pattern py

--- pattern.py
+++ pattern.py
@@ -6,10 +6,10 @@
 
 # program : 2
 
-# for i in range(2,7):
-#     for j in range(1,i):
-#         print(i+1, end =" ")
-#     print()
+for i in range(2,7):
+    for j in range(1,i):
+        print(i+1, end =" ")
+    print()
 
 
 # # pr : 3
