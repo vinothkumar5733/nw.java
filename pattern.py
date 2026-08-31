93e4ad69cc30c583c332ed29cd741bf7cd1732b8
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-08-31 21:20:06 +0530

    pattern.py

--- pattern.py
+++ pattern.py
@@ -1,15 +1,15 @@
-# for col in range(1,5):
-#     for row in range(1,col+1):
-#         print(col , end =" ")
-#     print()
+for col in range(1,5):
+    for row in range(1,col+1):
+        print(col , end =" ")
+    print()
 
 
 # program : 2
 
-for i in range(2,7):
-    for j in range(1,i):
-        print(i+1, end =" ")
-    print()
+# for i in range(2,7):
+#     for j in range(1,i):
+#         print(i+1, end =" ")
+#     print()
 
 
 # # pr : 3
