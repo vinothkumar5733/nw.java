85615da96b3d8cf344aa2262340cd585ed369b5b
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-09-01 23:00:44 +0530

    pattern3.py

--- pattern2.py
+++ pattern2.py
@@ -8,3 +8,9 @@ for row in range(1,6):
 #     for col in range(1,num): #1 2 3 4 5
 #         print(num-col, end=' ')
 #     print()
+
+no = 6
+for row in range(1, no):
+    for col in range(no - row):
+        print("*", end=" ")
+    print()
