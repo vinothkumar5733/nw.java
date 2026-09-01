fb47a5ba265bee0809533c1a9c04329367eaa7ff
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-09-01 22:41:07 +0530

    py.test

--- pattern2.py
+++ pattern2.py
@@ -1,10 +1,10 @@
-# for row in range(1,6):
-#     print(row)
-#     for col in range(1,row+1):
-#         print((6-col)*row, end = ' ')
+for row in range(1,6):
+    print(row)
+    for col in range(1,row+1):
+        print((6-col)*row, end = ' ')
 #     print()
 
-for num in range(6,1,-1):
-    for col in range(1,num): #1 2 3 4 5
-        print(num-col, end=' ')
-    print()
+# for num in range(6,1,-1):
+#     for col in range(1,num): #1 2 3 4 5
+#         print(num-col, end=' ')
+#     print()
