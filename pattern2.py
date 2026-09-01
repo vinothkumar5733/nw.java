569240f25cdec78d0a9ba485800b3cf5201d9fdc
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-09-01 22:00:10 +0530

    pattern2.py

--- pattern2.py
+++ pattern2.py
@@ -1,4 +1,5 @@
 for row in range(1,6):
-    for col in range(1,row+1):
-        print((5-col)*row, end = ' ')
-    print()
+    print(row)
+    # for col in range(1,row+1):
+    #     print((6-col)*row, end = ' ')
+    # print()
