ced01b7437ea64141869388086f30cf471725aba
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-09-02 20:39:03 +0530

    pattern day 3

--- pattern day3.py
+++ pattern day3.py
@@ -1,6 +1,22 @@
+# for row in range(1,6):
+#     for col in range(1, row):
+#         print("*", end = ' ')
+#     for star in range(1,6):
+#         print(1, end = ' ')
+#     print()
+
+# program :2
+
+# for row in range(1,6):
+#     for col in range(1,row+1):
+#         print("*", end =' ')
+#     for star in range(col+1):
+#         print(6-col, end = ' ')
+#     print()
+
 for row in range(1,6):
-    for col in range(1, row):
-        print("*", end = ' ')
-    for col in range(1,6):
-        print(1, end = ' ')
+    for col in range(1,6-row):
+        print("", end = ' ')
+    for col in range(1,row+1):    
+        print(6-col,end = ' ')    
     print()
