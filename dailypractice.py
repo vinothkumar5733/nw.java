4013faf32d427137296a83bae1a6f5212875360b
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-09-03 21:58:56 +0530

    daily practice

--- dailypractice.py
+++ dailypractice.py
@@ -1,6 +1,11 @@
 # 1)odd or even
 n = 6
-if n%2==0:
-    print("even")
-else:
+# if n%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+# w2)
+if n &1:
     print("odd")
+else: print("even")
