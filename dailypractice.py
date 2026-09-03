9457ec5b85cafbf4ade62c93bc1d8d71c58b0c4e
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-09-03 22:06:20 +0530

    daily practice

--- dailypractice.py
+++ dailypractice.py
@@ -1,11 +1,14 @@
 # 1)odd or even
-n = 6
+# n = 6
 # if n%2==0:
 #     print("even")
 # else:
 #     print("odd")
 
 # w2)
-if n &1:
-    print("odd")
-else: print("even")
+# if n &1:
+#     print("odd")
+# else: print("even")
+
+# 2) Check whether a number is prime
+
