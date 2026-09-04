28743f158c1bc197ea8b5f487d4d95a28214522a
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-09-04 11:59:29 +0530

    pattern day 4

--- patternday4.py
+++ patternday4.py
@@ -1,10 +1,10 @@
-# for row in range(1,5):
-#     for col in range(1,5):
-#         if row==1 or row ==4 or col == 1 or col == 4:
-#             print("*", end = " ")
-#         else:
-#             print(" ", end = " ")
-#     print()
+for row in range(1,5):
+    for col in range(1,5):
+        if row==1 or row ==4 or col == 1 or col == 4:
+            print("*", end = " ")
+        else:
+            print(" ", end = " ")
+    print()
 
 # for row in range(1,5):
 #     for col in range(1,5):
