bcdf96f0ca78789ce163740f772048e0239bff25
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-09-04 11:46:50 +0530

    pattern day 4

--- dailypractice.py
+++ dailypractice.py
@@ -1,4 +1,4 @@
-# 1)odd or even
+ # 1)odd or even
 # n = 6
 # if n%2==0:
 #     print("even")
@@ -6,9 +6,9 @@
 #     print("odd")
 
 # w2)
-# if n &1:
-#     print("odd")
-# else: print("even")
+if n &1:
+    print("odd")
+else: print("even")
 
 # 2) Check whether a number is prime
 
