1920f168b8fd93d02ac5171981959dfe28943917
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-09-04 12:03:03 +0530

    pattern day 4

--- patternday4.py
+++ patternday4.py
@@ -1,19 +1,19 @@
-for row in range(1,5):
-    for col in range(1,5):
-        if row==1 or row ==4 or col == 1 or col == 4:
-            print("*", end = " ")
-        else:
-            print(" ", end = " ")
-    print()
-
 # for row in range(1,5):
 #     for col in range(1,5):
-#         if row == 1 or row ==4 or col == 1 or col == 4:
-#             if(row == 1 and (col ==1 or col ==4)):
-#                 print(" ", end =" ")
-#             else:
-#                 print("*", end = " ")
-#         else:
+#         if row==1 or row ==4 or col == 1 or col == 4:
 #             print("*", end = " ")
-
+#         else:
+#             print(" ", end = " ")
 #     print()
+
+for row in range(1,5):
+    for col in range(1,5):
+        if row == 1 or row ==4 or col == 1 or col == 4:
+            if(row ==1 and col ==4):
+                print(" ", end =" ")
+            else:
+                print("*", end = " ")
+        else:
+            print(" ", end = " ")
+
+    print()
