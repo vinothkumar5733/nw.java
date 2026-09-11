37cb1b4c094995147921ce33a8015e5cf5349b1c
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-09-11 20:39:43 +0530

    remove space 1

--- removespace.py
+++ removespace.py
@@ -1,15 +1,15 @@
 # remove front_space
 
 
-sentence = '              today is my first day at the new job.'
-space =' '
-front_space= True
-for letter in sentence:
-    if letter == space and front_space == True:
-        continue
-    else:
-        front_space = False
-        print(letter,end='')
+# sentence = '              today is my first day at the new job.'
+# space =' '
+# front_space= True
+# for letter in sentence:
+#     if letter == space and front_space == True:
+#         continue
+#     else:
+#         front_space = False
+#         print(letter,end='')
 
 # remove back_space
 
