5d5643343d78dd37eb6a8d4ee00b9f977560848a
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-09-13 23:09:06 +0530

    remove space 1

--- patternday4.py
+++ patternday4.py
@@ -6,15 +6,15 @@ for row in range(1,5):
             print(" ", end = " ")
     print()
 
-# for row in range(1,5):
-#     for col in range(1,5):
-#         if row == 1 or row ==4 or col == 1 or col == 4:
-#             if(row ==4 and col ==4):
-#                 print(" ", end =" ")
-#             else:
-#                 print("*", end = " ")
-#         else:
-#             print(" ", end = " ")
+for row in range(1,5):
+    for col in range(1,5):
+        if row == 1 or row ==4 or col == 1 or col == 4:
+            if(row ==4 and col ==4):
+                print(" ", end =" ")
+            else:
+                print("*", end = " ")
+        else:
+            print(" ", end = " ")
 
 #     print()
 
