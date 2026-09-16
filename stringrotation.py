5b94a0e448cb181e7010bcbc30e4b0baa4c22997
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-09-16 22:24:42 +0530

    string rotate

--- stringrotation.py
+++ stringrotation.py
@@ -1,3 +1,4 @@
+# EX:01
 word = 'abcd'
 i = 1
 while i < len(word):
@@ -9,3 +10,25 @@ print(word[0])
 
 word = 'abcd'
 print(word[-1]+word[:-1])
+
+# EX:02
+
+s = 'VINOTH KUMAR'
+word = list(s)
+
+print(word)
+
+index = 0
+while index < len(word):
+    key = word[index]
+    count = 1
+    i = index + 1
+    while i < len(word):
+        if key != '*' and key == word[i]:
+            word[i] = '*'
+            count+=1
+        i+=1
+    if key != '*' and count==1:
+        print(key, 'is present', count, 'times')
+    index+=1
+#print(word)
