683e7733a1acf11dbe8900a77eed7a8d4dc76470
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-09-17 23:32:35 +0530

    string reverse

--- reversestring.py
+++ reversestring.py
@@ -6,3 +6,17 @@ while i >= 0:
     sentence2 = sentence2 + l[i] + " "
     i-=1
 print(sentence2)
+
+
+# ex2: camel case
+
+name = "vinoth kumar tuticorin"
+i = 0
+while i < len(name):
+    if name[i] == " ":
+        print(name[i+1].upper(),end ="")
+        i+=1
+    else:
+        print(name[i],end ="")
+        i+=1
+        
