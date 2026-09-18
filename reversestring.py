1e153eda3ac4cdaaba5a8b982cfbc9892664366b
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-09-18 23:06:31 +0530

    anagram

--- reversestring.py
+++ reversestring.py
@@ -1,11 +1,11 @@
-# sentence = "this is vinoth"
-# l = sentence.split()
-# i = len(l) - 1
-# sentence2 = ""
-# while i >= 0:
-#     sentence2 = sentence2 + l[i] + " "
-#     i-=1
-# print(sentence2)
+sentence = "this is vinoth"
+l = sentence.split()
+i = len(l) - 1
+sentence2 = ""
+while i >= 0:
+    sentence2 = sentence2 + l[i] + " "
+    i-=1
+print(sentence2)
 
 
 # ex2: camel case
