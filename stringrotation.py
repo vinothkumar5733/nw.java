128a29eb727aea734a02e897c9c02ff2c252bedf
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-09-19 23:30:51 +0530

    anagram

--- stringrotation.py
+++ stringrotation.py
@@ -19,16 +19,16 @@ print(word[0])
 # print(word)
 
 # index = 0
-# while index < len(word):
-#     key = word[index]
-#     count = 1
-#     i = index + 1
-#     while i < len(word):
-#         if key != '*' and key == word[i]:
-#             word[i] = '*'
-#             count+=1
-#         i+=1
-#     if key != '*' and count==1:
-#         print(key, 'is present', count, 'times')
-#     index+=1
-#print(word)
+while index < len(word):
+    key = word[index]
+    count = 1
+    i = index + 1
+    while i < len(word):
+        if key != '*' and key == word[i]:
+            word[i] = '*'
+            count+=1
+        i+=1
+    if key != '*' and count==1:
+        print(key, 'is present', count, 'times')
+    index+=1
+print(word)
