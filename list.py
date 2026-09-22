de5a3c8a1c77cb10c5f2aa6e5b26b7a44a9e6d0c
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-09-22 23:55:05 +0530

    list day 3

--- list.py
+++ list.py
@@ -4,14 +4,20 @@
 # print(l[0][0])
 # print(l[1][1])
 
+# LIST DAY 02
+
+# l1 = [10,20,30]
+# l2 = [40,50,60]
+# l3 = [70,80,90]
+
+# l = [l1,l2,l3]
+# result = sum(l1)+sum(l2)+sum(l3)
+# average = result/3
+# print(result)
+# print(average)
+
 # LIST DAY 03
 
-l1 = [10,20,30]
-l2 = [40,50,60]
-l3 = [70,80,90]
-
-l = [l1,l2,l3]
-result = sum(l1)+sum(l2)+sum(l3)
-average = result/3
-print(result)
-print(average)
+l1 = [10,20,10,20,30,40]
+l2 =list(set(l1))
+print(l2)
