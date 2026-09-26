b274a01fdc400f1f19cb3b8bd36ef2a36ef93ee1
vinothkumar5733 <kumarvinoth11971@gmail.com>
2026-09-26 22:45:27 +0530

    tuple day 02

--- pattern.py
+++ pattern.py
@@ -20,7 +20,7 @@ for row in range(1,7):
 
 # pr :4
 
-for row in range(2,7):
-    for col in range(1,row):
-        print(col*2, end=" ")
+# for row in range(2,7):
+#     for col in range(1,row):
+#         print(col*2, end=" ")
         
--- pattern2.py
+++ pattern2.py
@@ -4,10 +4,10 @@ for row in range(1,6):
         print((6-col)*row, end = ' ')
     print()
 
-for num in range(6,1,-1):
-    for col in range(1,num): #1 2 3 4 5
-        print(num-col, end=' ')
-    print()
+# for num in range(6,1,-1):
+#     for col in range(1,num): #1 2 3 4 5
+#         print(num-col, end=' ')
+#     print()
 
 no = 6
 for row in range(1, no):
